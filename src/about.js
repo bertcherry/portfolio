858eb261165bf17844804692a49a385389863575
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-05-15 21:46:23 -0700

    fix about load prevent default

--- src/about.js
+++ src/about.js
@@ -2,7 +2,8 @@ import { bioHeader, bioInfo } from './components';
 import { bio } from './content';
 
 //Generate bio details from content bio object
-function generateBio() {
+const generateBio = (e) => {
+    e.preventDefault();
     const content = document.getElementById('content');
     while (content.firstChild) {
         content.removeChild(content.lastChild);
@@ -17,20 +18,21 @@ function generateBio() {
     toolsTitle.textContent = 'Tools';
     const toolsList = document.createElement('ul');
     toolsList.classList.add('bio-list');
-    for (const tool in bio.tools) {
+    for (const tool of bio.tools) {
         const toolItem = document.createElement('li');
+        toolItem.textContent = tool;
         toolsList.appendChild(toolItem);
-    }
+    };
     bioTools.appendChild(toolsList);
     content.appendChild(bioTools);
 
-    const resume = document.createElement('div');
+    /* const resume = document.createElement('div');
     const resumeLink = document.createElement('a');
     resumeLink.classList.add('btn-resume');
     resumeLink.textContent = 'View Resume';
     resumeLink.href = bio.resume;
     resume.appendChild(resumeLink);
-    content.appendChild(resume);
+    content.appendChild(resume); */
 };
 
 export { generateBio };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { generateBio } from './about';
 import { generateHome } from './home';
 
-const navHome = document.getElementById('nav-home');
-navHome.addEventListener('click', generateHome);
+window.onload = () => {
+    const navHome = document.getElementById('nav-home');
+    navHome.addEventListener('click', generateHome);
 
-const navAbout = document.getElementById('nav-about');
-navAbout.addEventListener('click', generateBio);
+    const navAbout = document.getElementById('nav-about');
+    navAbout.addEventListener('click', generateBio);
 
-generateHome();
+    generateHome();
+}
