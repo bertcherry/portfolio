83ee0e1ad3e1d781beb57c5a492b82efb3517cec
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-05-15 17:13:14 -0700

    add about scripts

---
+++ src/about.js
@@ -0,0 +1,36 @@
+import { bioHeader, bioInfo } from './components';
+import { bio } from './content';
+
+//Generate bio details from content bio object
+function generateBio() {
+    const content = document.getElementById('content');
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+
+    content.appendChild(bioHeader);
+    content.appendChild(bioInfo);
+
+    const bioTools = document.createElement('div');
+    const toolsTitle = document.createElement('h2');
+    bioTools.appendChild(toolsTitle);
+    toolsTitle.textContent = 'Tools';
+    const toolsList = document.createElement('ul');
+    toolsList.classList.add('bio-list');
+    for (const tool in bio.tools) {
+        const toolItem = document.createElement('li');
+        toolsList.appendChild(toolItem);
+    }
+    bioTools.appendChild(toolsList);
+    content.appendChild(bioTools);
+
+    const resume = document.createElement('div');
+    const resumeLink = document.createElement('a');
+    resumeLink.classList.add('btn-resume');
+    resumeLink.textContent = 'View Resume';
+    resumeLink.href = bio.resume;
+    resume.appendChild(resumeLink);
+    content.appendChild(resume);
+};
+
+export { generateBio };
