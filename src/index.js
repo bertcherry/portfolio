eb221ff88a4d62c80fbc2d94abf153470512f9bf
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-07-31 18:11:15 -0700

    remove javascript loading content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './style.css';
-import { generateBio } from './about';
-import { generateHome } from './home';
-
-window.onload = () => {
-    const navHome = document.getElementById('nav-home');
-    navHome.addEventListener('click', generateHome);
-
-    const navAbout = document.getElementById('nav-about');
-    navAbout.addEventListener('click', generateBio);
-
-    generateHome();
-}
